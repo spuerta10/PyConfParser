3917ce3ad2be5d45d5d92852f9a05ea81aebc88c
spuerta10 <puertasantiago2002@gmail.com>
2025-03-12 15:35:28 -0500

    refactor: Refactored forcing method to inheritant classes

--- pyconfparser/config.py
+++ pyconfparser/config.py
@@ -1,26 +1,26 @@
 from abc import ABC, abstractmethod
-from pathlib import Path
 from os import path
+from pathlib import Path
+from typing import Any
 
 from pydantic import BaseModel
 
 
 class Config(ABC):
-    
-    
     conf_type: str
-    
 
-    @property
-    @abstractmethod
-    def conf_type(cls) -> None:
-        """Force inheritant classes to have conf_type class variable.
-        """
-        pass
+    ERROR_CONF_TYPE_NOT_DEFINED: str = "Can't instantiate {} class without conf_type attribute defined"
+    ERROR_PATH_DOES_NOT_EXIST: str = "The given path {} doesn't exist."
 
+    def __init_subclass__(cls, **kwargs: dict[str, Any]) -> None:
+        """Force inheritant classes to have conf_type class variable."""
+        if not hasattr(cls, "conf_type"):
+            raise NotImplementedError(cls.ERROR_CONF_TYPE_NOT_DEFINED.format(cls.__name__))
+
+        return super().__init_subclass__(**kwargs)
 
     @classmethod
-    def get_allowed_types(cls) -> dict[str, 'Config']:
+    def get_allowed_types(cls) -> dict[str, "Config"]:
         """Builds a dict of supported conf types extensions.
 
         Returns:
@@ -33,23 +33,20 @@ class Config(ABC):
             if hasattr(subclass, search_attribute):
                 allowed_types[getattr(subclass, search_attribute)] = subclass()
         return allowed_types
-    
 
-    def __init__(self, conf_path: None | str):
+    def __init__(self, conf_path: None | str = None):
         abs_path: str | None = path.abspath(conf_path) if conf_path is not None else None
         self._path: str | None = abs_path
-
 
     @property
     def path(self) -> str | None:
         """Absolute path of the passed conf file
 
         Returns:
-            str | None: Returns the absolute path of the passed conf file, 
+            str | None: Returns the absolute path of the passed conf file,
             none if the conf file path its not set.
         """
         return self._path
-
 
     @path.setter
     def path(self, new_path: str) -> None:
@@ -62,15 +59,15 @@ class Config(ABC):
             ValueError: In case the new path given doesn't exist.
         """
         if not Path(new_path).exists():
-            raise ValueError(f"The given path {new_path} doesn't exist.")
+            raise ValueError(self.ERROR_PATH_DOES_NOT_EXIST.format(new_path))
         self._path = path.abspath(new_path)
 
-
     @abstractmethod
-    def ingest_conf(self, schema: None | BaseModel = None) -> 'Config':
+    def ingest_conf(self, schema: None | BaseModel = None) -> "Config":
         """Ingests and makes every field of the conf file an object attribute.
         Args:
-            schema (None | BaseModel, optional): A pydantic class to validate that the content of a config file follows a given schema. 
+            schema (None | BaseModel, optional): A pydantic class to validate that the content
+            of a config file follows a given schema.
             Defaults to None.
 
         Returns:
--- pyconfparser/config_factory.py
+++ pyconfparser/config_factory.py
@@ -1,31 +1,33 @@
-from pathlib import Path
 from os import path
-
-from pyconfparser.config import Config
+from pathlib import Path
 
 from pydantic import BaseModel
+
+from pyconfparser.config import Config
 
 
 class ConfigFactory:
     @staticmethod
     def get_conf(conf_path: str, schema: None | BaseModel = None) -> Config:
-        """Returns the most appropiate conf object for the filepath given, 
+        """Returns the most appropiate conf object for the filepath given,
         parses the content of the file to a given schema if needed.
 
         Args:
             conf_path (str): The absolute or relative path to the config file. Defaults to None.
-            schema (None | BaseModel, optional): The schema to validate the content of the conf file. Defaults to None.
+            schema (None | BaseModel, optional): The schema to validate the content of the conf file.
+            Defaults to None.
 
         Returns:
             Conf: The implemented conf object that best fits to the file extension of conf file.
         """
+
         def init_conf_obj(conf_obj: Config, abs_path: str, schema: None | BaseModel) -> Config:
-            """Initializates conf object 
+            """Initializates conf object
 
             Args:
-                conf_obj (Conf): The implemented conf object that best fits to the file extension of conf file.
+                conf_obj (Conf): The implemented conf object that best fits to the file extension.
                 abs_path (str): The absolute path to the config file. Defaults to None.
-                schema (None | BaseModel): _description_
+                schema (None | BaseModel): The schema to validate the content of the conf file.
 
             Returns:
                 Conf: The initialized conf object.
@@ -33,13 +35,13 @@ class ConfigFactory:
             conf_obj.path = abs_path
             conf_obj.ingest_conf(schema)
             return conf_obj
-        
+
         abs_path = path.abspath(conf_path)  # absolute path of conf file
         assert Path(abs_path).exists(), f"The path {conf_path} doesn't exist."
-        
+
         _, file_extension = path.splitext(conf_path)
         types: dict[str, Config] = Config.get_allowed_types()
-        conf_obj: Config = types.get(file_extension)
-        
+        conf_obj: Config | None = types.get(file_extension)
+
         assert conf_obj is not None, f"{file_extension} type is not yet supported!"
         return init_conf_obj(conf_obj, abs_path, schema)
